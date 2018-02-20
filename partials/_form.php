bb29da35b59b04cebbac26a1ff6d058a11817c94
Brian Yingling <brian@jet.com>
2018-02-19 21:47:46 -0500

    change to single quotes

--- partials/_form.php
+++ partials/_form.php
@@ -1,27 +1,27 @@
-<form method="POST" action="index.php">
+<form method='POST' action='index.php'>
     
-    <div class="form-group row">
-        <label for="text" class="col-sm-2 col-form-label">Text to Translate</label>
-        <div class="col-sm-10">
-            <textarea name="text" cols="15" rows="10" class='form-control'><?= $form->prefill('text') ?></textarea>
+    <div class='form-group row'>
+        <label for='text' class='col-sm-2 col-form-label'>Text to Translate</label>
+        <div class='col-sm-10'>
+            <textarea name='text' cols='15' rows='10' class='form-control'><?= $form->prefill('text') ?></textarea>
         </div>
     </div>
     
-    <div class="form-group row">
-        <div class="col-sm-2 col-form-label">
-            <label for="suffix">Suffix</label>
+    <div class='form-group row'>
+        <div class='col-sm-2 col-form-label'>
+            <label for='suffix'>Suffix</label>
         </div>
-        <div class="col-sm-10">
-            <input type="radio" name="suffix" value="ay" <?= ($suffix == 'ay' || !isset($suffix)) ? 'checked' : ''?>> "ay"
-            <input type="radio" name="suffix" value="a" <?= ($suffix == 'a') ? 'checked' : ''?>> "a"
+        <div class='col-sm-10'>
+            <input type='radio' name='suffix' value='ay' <?= ($suffix == 'ay' || !isset($suffix)) ? 'checked' : ''?>> "ay"
+            <input type='radio' name='suffix' value='a' <?= ($suffix == 'a') ? 'checked' : ''?>> "a"
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-sm-2">
+    <div class='row'>
+        <div class='col-sm-2'>
             Rules
         </div>
-        <div class="col-sm-10">
+        <div class='col-sm-10'>
             <p>
                 Words that begin with a single consonant 
                 shift the first letter to the end and 
@@ -40,24 +40,24 @@
         </div>
     </div>
 
-    <div class="form-group row">
-        <div class="col-sm-2 col-form-label">
-            <label for="short">Optional Rules</label>
+    <div class='form-group row'>
+        <div class='col-sm-2 col-form-label'>
+            <label for='short'>Optional Rules</label>
         </div>
-        <div class="col-sm-10">
-            <input type="checkbox" name="short" <?= $short ? 'checked' : '' ?>>
+        <div class='col-sm-10'>
+            <input type='checkbox' name='short' <?= $short ? 'checked' : '' ?>>
             Words that are shorter than three characters are left as is. 
             Example: <em>an => an</em>
         </div>
     </div>
     
-    <div class="form-group row">
-        <div class="col-sm-2">
-            <button type="submit" name="submit" class='btn btn-primary'>Translate</button>
+    <div class='form-group row'>
+        <div class='col-sm-2'>
+            <button type='submit' name='submit' class='btn btn-primary'>Translate</button>
         </div>
         <?php if ($translated) : ?>
-            <div class="col-sm-10">
-                <div class="translated alert alert-success">
+            <div class='col-sm-10'>
+                <div class='translated alert alert-success'>
                     <?= $translated ?>
                 </div>
             </div>
